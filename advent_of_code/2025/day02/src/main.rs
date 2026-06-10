38f943a9d646f5a1c4a139ba48fec4a8a210a53c
emrutsdmr <emrullahtsdmr@gmail.com>
2026-06-10 20:22:50 +0200

    Solve Day 2 Part 2 with repeating substring checks

--- advent_of_code/2025/day02/src/main.rs
+++ advent_of_code/2025/day02/src/main.rs
@@ -14,8 +14,8 @@ fn parse_instruction(line: &str) -> Vec<(u64, u64)> {
 }
 
 fn part1(ranges: &Vec<(u64, u64)>) -> u64 {
-
   let mut sum: u64 = 0;
+
   for &(start, end) in ranges {
     for id in start..=end {
       if is_invalid_part1(id) {
@@ -23,7 +23,6 @@ fn part1(ranges: &Vec<(u64, u64)>) -> u64 {
       }
     }
   }
-
   sum
 }
 
@@ -40,16 +39,52 @@ fn is_invalid_part1(id: u64) -> bool {
   false
 }
 
-fn is_invalid_part2(id: u64) -> bool {
+fn part2(ranges: &Vec<(u64, u64)>) -> u64 {
+  let mut sum: u64 = 0;
+
+  for &(start, end) in ranges {
+    for id in start..=end {
+      if is_invalid_part2(id) {
+        sum += id;
+      }
+    }
+  }
+  sum
 }
 
+fn is_invalid_part2(id: u64) -> bool {
+  let id_str = id.to_string();
+  let len = id_str.len();
+  let mut group = 1;
+  let mut index = 0;
+  let mut invalid: bool;
 
-fn part2(input: &str) -> u64 {
+  while group != len {
+    if len % group != 0 {
+      group += 1;
+      continue;
+    }
+    invalid = true;
+
+    while (index + group + group) <= len {
+      if &id_str[index..(index + group)] == &id_str[(index + group)..(index + group + group)] {
+        index += group;
+      }
+      else {
+        invalid = false;
+        break;
+      }
+    }
+    if invalid { return true}
+    group += 1;
+    index = 0;
+  }
+  false
 }
 
 fn main() {
   let input = fs::read_to_string("input.txt").expect("Failed to read input file");
   let ranges: Vec<(u64, u64)> = parse_instruction(&input);
-//  println!("Part 1 Solution: {}", part1(&ranges));
+  println!("Part 1 Solution: {}", part1(&ranges));
   println!("Part 2 Solution: {}", part2(&ranges));
 }
