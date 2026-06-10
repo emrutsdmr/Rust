5f4f97d90cb534f71f414cb90456fbb0507144fb
emrutsdmr <emrullahtsdmr@gmail.com>
2026-06-10 12:15:39 +0200

    Refactor Day 2 Part 1 and add Part 2 skeleton

--- advent_of_code/2025/day02/src/main.rs
+++ advent_of_code/2025/day02/src/main.rs
@@ -1,6 +1,33 @@
 use std::fs;
 
-fn is_invalid(id: u64) -> bool {
+fn parse_instruction(line: &str) -> Vec<(u64, u64)> {
+  line.trim()
+    .split(',')
+    .map(|s| {
+        let (start, end) = s.split_once('-').unwrap();
+        (
+          start.parse().unwrap(),
+          end.parse().unwrap(),
+        )
+    })
+    .collect()
+}
+
+fn part1(ranges: &Vec<(u64, u64)>) -> u64 {
+
+  let mut sum: u64 = 0;
+  for &(start, end) in ranges {
+    for id in start..=end {
+      if is_invalid_part1(id) {
+        sum += id;
+      }
+    }
+  }
+
+  sum
+}
+
+fn is_invalid_part1(id: u64) -> bool {
   let id_str = id.to_string();
 
   if id_str.len() % 2 == 0 {
@@ -13,31 +40,16 @@ fn is_invalid(id: u64) -> bool {
   false
 }
 
+fn is_invalid_part2(id: u64) -> bool {
+}
+
+
+fn part2(input: &str) -> u64 {
+}
+
 fn main() {
   let input = fs::read_to_string("input.txt").expect("Failed to read input file");
-
-  let ranges: Vec<(u64, u64)> = input
-    .trim()
-    .split(',')
-    .map(|s| {
-      let (start, end) = s.split_once('-').unwrap();
-//      println!("start = {:?}", start);
-//      println!("end   = {:?}", end);
-      (
-          start.parse().unwrap(),
-          end.parse().unwrap(),
-      )
-    })
-    .collect();
-
-    let mut sum: u64 = 0;
-    for (start, end) in ranges {
-      for i in start..=end {
-        if is_invalid(i) {
-          sum += i;
-        }
-      }
-    }
-
-  println!("{}", sum);
+  let ranges: Vec<(u64, u64)> = parse_instruction(&input);
+//  println!("Part 1 Solution: {}", part1(&ranges));
+  println!("Part 2 Solution: {}", part2(&ranges));
 }
