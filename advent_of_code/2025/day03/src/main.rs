1d0958d26ac60e970322c0641d7f28dce80f39bd
emrutsdmr <emrullahtsdmr@gmail.com>
2026-06-12 10:07:45 +0200

    Initial solution using incorrect input data format

--- advent_of_code/2025/day03/src/main.rs
+++ advent_of_code/2025/day03/src/main.rs
@@ -44,7 +44,58 @@ fn part1(banks: &Vec<Vec<u8>>) -> u64 {
   sum
 }
 
+fn part2(banks: &Vec<Vec<u8>>) -> u64 {
+  let mut sum: u64 = 0;
+
+  for bank in banks {
+    let len = bank.len();
+
+    assert!(len == 15);
+//TODO go through the code for [2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 7, 8]
+    let mut num: u64 = 0;
+    let mut count = 3;
+    let mut ind = 0;
+    let mut chosen = bank[0];
+    let mut chosen_ind = 0;
+
+    println!("{:?}", bank);
+    println!("{}", "434234234278");
+    while count > 0 && ind < 12{
+      let tmp_count = count;
+      for i in 1..=tmp_count {
+        if chosen < bank[ind + i] {
+          chosen = bank[ind + i];
+          chosen_ind = ind + i;
+          count -= 1;
+          println!("chosen: {:?} count: {:?}", chosen, count);
+        }
+      }
+      num = num * 10 + chosen as u64;
+      println!("Number: {:?}", num);
+      if count >= 0 {
+        if  ind < chosen_ind && chosen_ind + 1 < 15{
+          ind = chosen_ind + 1;
+          chosen = bank[ind];
+        }
+        else{
+          ind += 1;
+          chosen = bank[ind];
+        }
+      }
+    }
+    let mut i = 0;
+    while ind < 12 && ind +i < 15 {
+      num = num * 10 + bank[ind + i] as u64;
+      println!("{:?} {:?} {:?}", ind + i, bank[ind + i], num);
+      i += 1;
+    }
+    println!("{:?}", num);
+    sum += num;
+  }
+  sum
+}
+
 fn main() {
   let banks = parse("input.txt");
-  println!("{}", part1(&banks));
+  println!("{}", part2(&banks));
 }
