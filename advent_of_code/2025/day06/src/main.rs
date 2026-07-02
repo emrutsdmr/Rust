41b03bbe507c44f1157731954e0970ad68b531f8
emrutsdmr <emrullahtsdmr@gmail.com>
2026-07-02 17:08:50 +0200

    implement part 1 evaluation and part 2 grid transformation

--- advent_of_code/2025/day06/src/main.rs
+++ advent_of_code/2025/day06/src/main.rs
@@ -16,7 +16,6 @@ fn parse(path: &str) -> Vec<Problem> {
   let mut problems = Vec::new();
   let mut start = None;
 
-
   for c in 0..=w {
 //    let mut blank = true;
 //
@@ -32,7 +31,6 @@ fn parse(path: &str) -> Vec<Problem> {
 //    }
 
     //.all(...) : Is the condition true for every value?
-
     let blank = c == w || (0..h).all(|r| rows[r].get(c).copied().unwrap_or(' ') == ' ');
 
     match (start, blank) {
@@ -64,6 +62,21 @@ fn parse(path: &str) -> Vec<Problem> {
 }
 
 fn eval_part1(problem: &Problem) -> i64 {
+  let mut result: i64 = 0;
+  match problem.op {
+    '+' => result = problem.rows
+            .iter()
+            .map(|s| s.trim().parse::<i64>().unwrap())
+            .sum(),
+
+    '*' => result = problem.rows
+            .iter()
+            .map(|s| s.trim().parse::<i64>().unwrap())
+            .product(),
+
+    _   => println!("Invalid Operator!"),
+  }
+  result
 }
 
 fn part1(data: &[Problem]) -> i64 {
@@ -71,6 +84,21 @@ fn part1(data: &[Problem]) -> i64 {
 }
 
 fn eval_part2(problem: &Problem) -> i64 {
+  let mut new_rows: Vec<String> = Vec::new();
+
+  for row in problem.rows.iter() {
+    let len = row.len();
+    for i in 0..len {
+      let ch = row.as_bytes()[i] as char;
+      match new_rows.get_mut(i) {
+        Some(s) => s.push(ch),
+        None => new_rows.push(ch.to_string()),
+      }
+    }
+  }
+
+  new_rows.reverse();
+  eval_part1(&Problem{op: problem.op, rows: new_rows})
 }
 
 fn part2(data: &[Problem]) -> i64 {
