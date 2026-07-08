8a10bfe21f3e99e547cc0258f14cc062fca372f5
emrutsdmr <emrullahtsdmr@gmail.com>
2026-07-08 14:47:20 +0200

    finish day 7 part 2

--- advent_of_code/2025/day07/src/main.rs
+++ advent_of_code/2025/day07/src/main.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use std::collections::HashMap;
 
 fn parse(path: &str) -> Vec<Vec<char>> {
   fs::read_to_string(path)
@@ -40,7 +41,47 @@ fn part1(data: &[Vec<char>]) -> usize {
 }
 
 fn part2(data: &[Vec<char>]) -> usize {
-    todo!()
+  let mid = data[0].len() / 2;
+  let mut cp_data = data.to_vec();
+  let mut memo: HashMap<(usize, usize), usize> = HashMap::new();
+
+  cp_data[1][mid] = '|';
+
+  let rows = data.len();
+  let cols = data[0].len();
+
+  return part2_recursive(&cp_data, &mut memo, 1, mid, rows, cols);
+}
+
+fn part2_recursive(data: &[Vec<char>],
+                   memo: &mut HashMap<(usize, usize), usize>,
+                   i: usize, j: usize,
+                   rows: usize, cols: usize) -> usize{
+  if let Some(&value) = memo.get(&(i,j)) {
+    return value;
+  }
+
+  if i == rows - 1 {
+    return 1;
+  }
+
+  let mut ans;
+
+  if data[i + 1][j] == '^' {
+    ans = 0;
+    if j > 0 {
+      ans += part2_recursive(data, memo, i + 1, j - 1, rows, cols);
+    }
+
+    if j + 1 < cols {
+      ans += part2_recursive(data, memo, i + 1, j + 1, rows, cols);
+    }
+  }
+  else {
+    ans = part2_recursive(data, memo, i + 1, j, rows, cols);
+  }
+  memo.insert((i,j), ans);
+  ans
 }
 
 fn main() {
